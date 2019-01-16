1e056f4d72e28ef12ed3f2f222b60e6f49fea443
Bobby L <rioted@gmail.com>
2019-01-15 20:49:31 -0600

    Fixed some things

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ console.log('Running app.js');
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const _ = require('lodash');
-const yargs = require('yargs');
 const taskdb = './db/tasks.db';
+
+var argv = require('yargs').argv;
+//	.alias('a', 'alljs')
+//	.describe('a', 'show all')
+//	.usage('Usage: $0 a')
+//	.command('alljs', 'show all')
 
 const funcs = require('./crud.js');
 
-const argv = yargs.argv;
 var command = argv._[0];
 
-console.log('Running command:',command);
+console.log('Running command:',argv);
 
 if (command === 'alljs') {
 	funcs.alljs();
